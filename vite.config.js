06c4920e590df996ba734fc12893558038ce0a8d
Visal <sorviksalremix2019@gmail.com>
2026-05-07 14:57:04 +0700

    resolve merge conflict

--- vite.config.js
+++ vite.config.js
@@ -17,12 +17,6 @@ export default defineConfig({
   },
   server: {
     host: true,
-<<<<<<< HEAD
     allowedHosts: ['sorvisal.site']
   }
 })
-=======
-    allowedHosts: ['sssss.sorvisal.site']
-  }
-})
->>>>>>> 212a3df9d1b10b1ad20f9407a8628cf00e8d5f66
