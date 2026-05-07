212a3df9d1b10b1ad20f9407a8628cf00e8d5f66
sorviksal <166300177+sorviksal@users.noreply.github.com>
2026-05-07 14:39:55 +0700

    Update vite.config.js
    yu

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,4 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-// import path from "path";
 
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   resolve: {
-//     alias: {
-//       // eslint-disable-next-line no-undef
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-//   server:{
-//     host: true,
-//     allowedHosts: ['portfolio.sorvisal.site']
-//   }
-// });
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -35,6 +18,6 @@ export default defineConfig({
   },
   server: {
     host: true,
-    allowedHosts: ['sorvisal.site']
+    allowedHosts: ['sssss.sorvisal.site']
   }
 })
